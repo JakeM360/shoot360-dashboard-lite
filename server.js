c0db88dcd04124cdc1468d9f8441d56fa41196dc
JakeM360 <jakeob.maygra@shoot360.com>
2025-08-06 16:56:07 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require("express");
 const axios   = require("axios");
 const cors    = require("cors");
 const app = express();
-const GHL_API_KEY = process.env.GHL_API_KEY;
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
